69a57ce86b88446640090d0ba3ac0f645eb0ab03
200~Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-05-28 20:11:46 +0530

    made changes

--- strings.py
+++ strings.py
@@ -1,3 +1,11 @@
 a = "jeman kumar"
-a.capitalize() # Doesn't return anything
+a.capitalize() # Returns a capitalized string. It doesn't change the original string
 print(a.capitalize()) #Gives 'Jeman kumar' not 'Jeman Kumar'
+
+b = 'JEMAN KUMAR'
+print(b.lower())
+
+c = "   Jeman    "
+c.strip() 
+print(c) # Doesn't change the string.
+print(c.strip())
