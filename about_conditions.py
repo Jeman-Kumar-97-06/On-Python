32ed5c931a8d0bd3501c8ce3811340e40ea54543
200~Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-06-06 17:10:07 +0530

    Written code for loops:

---
+++ about_conditions.py
@@ -0,0 +1,19 @@
+age = 18
+if age >= 18:
+    print('You are an adult')
+
+num = 5
+if (num % 2) == 0:
+    print("Even")
+else : 
+    print("Odd")
+
+marks = 85
+if marks >= 90:
+    print("A Grade")
+elif marks >= 75:
+    print("B Grade")
+elif marks >= 65:
+    print("C Grade")
+else:
+    print("Fail")
