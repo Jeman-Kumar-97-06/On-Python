8e26c78072e6d9a8df3638aab7a6f11732b8dc81
200~Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-06-06 16:16:56 +0530

    Made changes to code about operators

---
+++ about_ops.py
@@ -0,0 +1,22 @@
+#ARITHMETIC OPERATORS : 
+print('2'+'jeman')
+print(2 * 'jeman')
+
+# print('jeman'-'n')
+print(10-1)
+
+print(10/3) #Gives 3.333
+print(10//3) #Gives 3
+
+print(10%3) #Gives remainder 1
+
+#COMPARISION OPERATORS : 
+print(True=="True") #Gives False
+print(10%2 == 0) 
+
+#LOGICAL OPERATORS : 
+#BITWISE OPERATORS :
+#ASSIGNMENT OPERATORS : 
+#IDENTITY OPERATORS : 
+#MEMBERSHIP OPERATORS : 
+
