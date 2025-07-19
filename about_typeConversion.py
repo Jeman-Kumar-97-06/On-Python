b21602890a65ce878f62b60875909d065b541acf
200~Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-07-19 14:00:47 +0530

    made changes

--- about_typeConversion.py
+++ about_typeConversion.py
@@ -6,3 +6,11 @@ print(f"Hello {name}")
 #Ex 2:
 name = input("Please enter:")
 print(name)
+
+#Type Conversion : 
+
+num = 12345
+#print(len(num)) will give error.
+print(len(str(num)))
+
+print(type(num))#--> <class 'int'>
