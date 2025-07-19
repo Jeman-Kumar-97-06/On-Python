e6f1dde3eeba58ea8982cd4e3eeff492dc298f29
200~Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-07-19 14:46:59 +0530

    made changes

--- about_typeConversion.py
+++ about_typeConversion.py
@@ -7,10 +7,22 @@ print(f"Hello {name}")
 name = input("Please enter:")
 print(name)
 
-#Type Conversion : 
+#Type Conversion (TypeCasting) : 
 
 num = 12345
 #print(len(num)) will give error.
 print(len(str(num)))
 
 print(type(num))#--> <class 'int'>
+
+print(int('123')+int('123'))
+
+#print(int('123')+'456') gives error
+
+print(bool(1)) #Gives True
+
+print(str(123))
+
+print(float(123)) #Gives 123.0
+
+print(int(12.5)) #Gives 12
