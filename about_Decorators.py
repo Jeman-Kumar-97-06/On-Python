64344e24cbc7f93e7aafcdbe6071a85ed5ef63e7
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-11-07 21:50:48 +0530

    made changes

---
+++ about_Decorators.py
@@ -0,0 +1,66 @@
+#First Class Functions : 
+#   Treated as first-class citizens.
+#   They are just handled how strings, ints are handled.
+#   They can be assigned to values : 
+def greet(name):
+    return f'hello, {name}'
+
+my_greet_func = greet
+print(my_greet_func('jeman'))
+
+#   They can be passed as args to other functions:
+def apply_op(func,value):
+    return func(value)
+
+def square(x):
+    return x*x
+
+print(apply_op(square,8))
+
+#   They can be return values of other functions.
+#   They can also be elements of lists, dicts or sets.
+
+#Closure:
+def outer_f(msg):
+    message = msg
+    def inner_function():
+        print(message)
+    return inner_function
+
+my_func = outer_f('HI') # outer_f is done with execution. The memory is cleared. But the inner function still remembers 'message'
+my_func()
+
+#Decorators
+# A Decorator is a function fx that takes fy as arg and adds some
+# xtra functionality and returns a new fz without altering the 
+# original fy that we sent as the arg.
+
+#Example1 :
+def decorator_1(og_f):
+    print(og_f) #-------------------------------> LocationX
+    def wrapper_fun():
+        return og_f()
+        # return og_f
+    return wrapper_fun
+
+def display():
+    print("Display function ran!")
+
+decorated_display = decorator_1(display)
+print(decorated_display()) #--------------------> LocationX
+decorated_display()
+
+#Example2 : Same as the above. But with Decorator syntax:
+def decorator_2(og_f):
+    def wrapper_f():
+        print("Wrapper function ran!")
+        return og_f()
+    return wrapper_f
+
+@decorator_2
+def display():
+    print("Display function ran yo!")
+
+display()
+
+#decorator syntax:
