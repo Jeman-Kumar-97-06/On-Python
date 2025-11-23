17ab322793f43957667ddd6ea3c618a502add63d
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-11-23 16:39:30 +0530

    made chagnes

--- Requests_Module/p1.py
+++ Requests_Module/p1.py
@@ -27,3 +27,49 @@ print(response.json()) #Convert the respones to a dictionary
 print(response.json()['title'])
 
 print(response.headers['Content-Type'])
+
+response2 = requests.get(
+    'https://api.github.com/search/repositories',
+    params={"q":"language:python","sort":"stars","order":"desc"} #Sending queries
+)
+"""
+    You can even pass the params as a tuple : 
+    requests.get(
+        "https://api.github.com/search/repositories",
+        [("q", "language:python"), ("sort", "stars"), ("order", "desc")],
+    )
+
+    OR as a bytes:
+    requests.get(
+        "https://api.github.com/search/repositories",
+        params=b"q=language:python&sort=stars&order=desc",
+    )
+"""
+
+print("\n\n")
+json_response = response2.json()
+popular_repos = json_response['items']
+for repo in popular_repos[:3]:
+    print(f"Name:{repo['name']}")
+    print(f"Description:{repo['description']}")
+    print(f"Stars:{repo['stargazers_count']}\n")
+
+
+response3 = requests.get(
+    'https://api.github.com/search/repositories',
+    params={'q':'"real python"'},
+    headers={"Accept":"application/vnd.github.text-match+json"},
+)
+
+json_response = response3.json()
+first_repository = json_response['items']
+print(first_repository[0]['text_matches'][0]['matches'])
+
+"""
+Sending post request with a body : 
+requests.post('urlgoeshere',data={"key":"value"}) --> use data when the Content-Type u want to send is : application/x-www-form-urlencoded
+requests.post('urlgoeshere',json={"key":"value"}) --> use json when the Content-Type u want to send is : JSON
+"""
+
+
+
